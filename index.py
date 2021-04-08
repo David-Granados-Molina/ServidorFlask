72675f9fbddf27bcd3533c3207d33a38ad60e97d
practicasfrontend <practicasfrontend@iMac-de-Fer.local>
2021-04-08 11:36:52 +0200

    Cambios diccionario

--- index.py
+++ index.py
@@ -5,16 +5,22 @@ app = Flask(__name__)
 
 @app.route('/componente1')
 def data():
-    dictionary = {'gold': 'Todo Incluido Be Gold', 'title': 'Puente Real', 'subtitle': 'Lovers Club',
-                  'icons': 'class:"icons"'}
+    dictionary = {
+        'gold': 'Todo Incluido Be Gold',
+        'title': 'Puente Real',
+        'subtitle': 'Lovers Club',
+        'icons': 'class:"icons"'
+    }
+
+    datos = []
+
+    for valor in dictionary.values():
+
+        datos.append(valor)
+
 
     return render_template(
-        'index.html',
-        gold=dictionary['gold'],
-        title=dictionary['title'],
-        subtitle=dictionary['subtitle'],
-        icons=dictionary['icons']
-    )
+        'index.html', datos=datos)
 
 
 if __name__ == '__main__':
